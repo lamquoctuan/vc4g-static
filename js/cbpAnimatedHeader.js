d46b00a7976eb23f965d4f89b3b3d57c2edb5fab
Tuan Lam <tuanlam@qasymphony.com>
2015-07-17 16:34:19 +0000

    add how to sell page

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -4,25 +4,25 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
 var cbpAnimatedHeader = (function() {
 
-	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-default' ),
-		didScroll = false,
-		changeHeaderOn = 300;
+    var docElem = document.documentElement,
+        header = document.querySelector( '.navbar-default' ),
+        didScroll = false,
+        changeHeaderOn = 300;
 
-	function init() {
-		window.addEventListener( 'scroll', function( event ) {
-			if( !didScroll ) {
-				didScroll = true;
-				setTimeout( scrollPage, 250 );
-			}
-		}, false );
-	}
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
 
     function scrollPage() {
         $(function(){
@@ -37,10 +37,11 @@ var cbpAnimatedHeader = (function() {
         });
     }
 
-	function scrollY() {
-		return window.pageYOffset || docElem.scrollTop;
-	}
 
-	init();
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
 
 })();
